5a75311a7d58bdeeb3fe68619435c82d442d17e3
Nusrat Jabeen <njs.khan.nk@gmail.com>
2026-05-14 23:22:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// function calculateBill() {
+
+//   let units = document.getElementById("units").value;
+
+//   if(units === ""){
+//     document.getElementById("result").innerHTML = "Please enter units";
+//     return;
+//   }
+
+//   let bill = 0;
+
+//   if(units <= 100){
+//     bill = units * 5;
+//   }
+//   else if(units <= 200){
+//     bill = (100 * 5) + ((units - 100) * 8);
+//   }
+//   else{
+//     bill = (100 * 5) + (100 * 8) + ((units - 200) * 10);
+//   }
+
+//   document.getElementById("result").innerHTML =
+//     "Total Bill = Rs. " + bill;
+// }
+
+
+function calculateBill() {
+  let units = document.getElementById("units").value;
+  let bill = 0;
+
+  if (units <= 100) {
+    bill = units * 10;
+  } 
+  else if (units <= 200) {
+    bill = (100 * 10) + (units - 100) * 15;
+  } 
+  else if (units <= 300) {
+    bill = (100 * 10) + (100 * 15) + (units - 200) * 20;
+  } 
+  else {
+    bill = (100 * 10) + (100 * 15) + (100 * 20) + (units - 300) * 25;
+  }
+
+  document.getElementById("result").innerHTML =
+    "Your Bill: Rs " + bill;
+}
+
+
+
+
+
+
+
+
+
